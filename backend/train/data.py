4d7562a47a419a32cff1f6ec733e9199d82b5f42
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-07-07 08:19:01 +0800

    rm backend/

--- backend/train/data.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-
-# Always change together with Android `HttpClient.ServerData`.
-@dataclass
-class ServerData:
-    status: str
-    session_id: int | None
-    port: int | None
--- backend/train/urls.py
+++
@@ -1,4 +0,0 @@
-from django.urls import path
-from train.views import *
-
-urlpatterns = [path("advertised", advertise_model), path("server", request_server)]
